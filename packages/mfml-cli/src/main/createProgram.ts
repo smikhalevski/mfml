9220f7d6d29d23ae96a3e6cce4dee1a2759c3ba4
Savva Mikhalevski <smikhalevski@gmail.com>
2021-08-11 20:48:27 +0300

    Fixed runtime deps

--- packages/mfml-cli/src/main/createProgram.ts
+++ packages/mfml-cli/src/main/createProgram.ts
@@ -12,12 +12,12 @@ export function createProgram(): Command {
   program.version(packageJson.version);
   program.description(packageJson.description);
 
-  program.option(`-c, --${CliOptionKey.CONFIG} <path>`, 'the config path');
-  program.option(`-i, --${CliOptionKey.INCLUDE} <path...>`, 'the pattern of file paths that must be included in the compilation');
+  program.option(`-c, --${CliOptionKey.CONFIG} <file>`, 'the config path');
+  program.option(`-i, --${CliOptionKey.INCLUDE} <pattern...>`, 'the glob pattern of file paths that must be included in the compilation');
   program.option(`-o, --${CliOptionKey.OUT_DIR} <dir>`, 'the output folder for all emitted files');
   program.option(`-d, --${CliOptionKey.ROOT_DIR} <dir>`, 'the root folder from which included file paths are resolved');
-  program.option(`-p, --${CliOptionKey.PRESET} <path...>`, 'the path of a configuration preset');
-  program.option(`-a, --${CliOptionKey.ADAPTER} <path>`, 'the path to a compilation adapter');
+  program.option(`-p, --${CliOptionKey.PRESET} <preset...>`, 'the path of a configuration preset');
+  program.option(`-a, --${CliOptionKey.ADAPTER} <adapter>`, 'the path to a compilation adapter');
   program.option(`-l, --${CliOptionKey.DEFAULT_LOCALE} <locale>`, 'default locale');
   program.option(`-t, --${CliOptionKey.TS}`, 'produce TypeScript typings for message functions');
 
