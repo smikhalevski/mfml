07c8112b2a6fc4663563e50089c2426e8bf011ca
Savva Mikhalevski <smikhalevski@gmail.com>
2021-12-23 01:23:24 +0300

    Limited entities decoding with preset

--- packages/cli/src/main/presets/html.ts
+++ packages/cli/src/main/presets/html.ts
@@ -1,9 +1,3 @@
 import {htmlParserOptions} from 'tag-soup';
-import {decodeHtml} from 'speedy-entities/lib/full';
 
-export default {
-  ...htmlParserOptions,
-
-  decodeText: decodeHtml,
-  decodeAttribute: decodeHtml,
-};
+export default htmlParserOptions;
